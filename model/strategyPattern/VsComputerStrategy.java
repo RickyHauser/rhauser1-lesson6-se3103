46e0724310e2bf1f7cfe8b3bde8832779556eb5f
Ricky Hauser <rmh5600@gmail.com>
2024-03-16 00:57:56 -0500

    20vsComputerStrategy

--- model/strategyPattern/VsComputerStrategy.java
+++ model/strategyPattern/VsComputerStrategy.java
@@ -1,10 +1,39 @@
 package model.strategyPattern;
 
+import model.Marking;
+import model.TicTacToeGame;
+
 public class VsComputerStrategy implements PlayStrategy{
+
+    private TicTacToeGame game;
+
+    VsComputerStrategy(TicTacToeGame game) {
+        this.game = game;
+    }
 
     @Override
     public void play(int position) {
-        
+        game.getBoard()[position] = game.getTurn();
+        game.incMoves();
+        game.setWinner();
+        if (game.getWinner() != null) return;
+
+        //computer player
+        game.ChangeTurns();
+        int pos = computerPick();
+        game.getBoard()[pos] = game.getTurn();
+        game.incMoves();
+    }
+    private int computerPick() {
+        int pos = -1;
+        for (int i = 0; i < game.getBoard().length; i++){
+            if (game.getBoard()[i] == Marking.U) {
+                pos = i;
+                break;
+            }
+        }
+        assert pos >= 0 : "Invalid position from computerPick()";
+        return pos;
     }
     
 }
